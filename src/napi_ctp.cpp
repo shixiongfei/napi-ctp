87cbbd1ce09f66341b4a16d6ac92d85f7f89cca2
shixiongfei <jenson.shixf@gmail.com>
2023-03-20 18:11:42 +0800

    行情消息转换JS对象

--- src/napi_ctp.cpp
+++ src/napi_ctp.cpp
@@ -10,9 +10,12 @@
  */
 
 #include "napi_ctp.h"
+#include <string.h>
 
 #ifdef _WIN32
 #include <Windows.h>
+#else
+#include <iconv.h>
 #endif
 
 static long volatile sequenceLastId = 0;
@@ -71,4 +74,90 @@ napi_value createInstance(napi_env env, napi_callback_info info,
   }
 
   return instance;
+}
+
+static char *toUTF8(const char *codepage, const char *mbstr, int len,
+                    char *utf8str) {
+#ifndef _WIN32
+  iconv_t cd;
+  size_t inlen = len, outlen = len * sizeof(int);
+
+  cd = iconv_open("UTF-8//TRANSLIT", codepage);
+
+  if (((iconv_t)(-1)) == cd)
+    return 0;
+
+  iconv(cd, (char **)&mbstr, &inlen, &utf8str, &outlen);
+  iconv_close(cd);
+
+  return utf8str;
+#else
+  int wlen;
+  dynarray(wchar_t, wcstr, len + 1);
+
+  memset(wcstr, 0, (len + 1) * sizeof(wchar_t));
+
+  wlen = MultiByteToWideChar(CP_ACP, 0, mbstr, len, wcstr, len + 1);
+  WideCharToMultiByte(CP_UTF8, 0, wcstr, wlen, utf8str, len * sizeof(wchar_t),
+                      NULL, NULL);
+  return utf8str;
+#endif
+}
+
+napi_status objectSetString(napi_env env, napi_value object, const char *name,
+                            const char *string) {
+  napi_status status;
+  napi_value value;
+  int len = (int)strlen(string);
+  dynarray(char, utf8str, len * 6 + 1);
+
+  status = napi_create_string_utf8(env, toUTF8("GBK", string, len, utf8str),
+                                   NAPI_AUTO_LENGTH, &value);
+  assert(status == napi_ok);
+
+  return napi_set_named_property(env, object, name, value);
+}
+
+napi_status objectSetInt32(napi_env env, napi_value object, const char *name,
+                           int32_t number) {
+  napi_status status;
+  napi_value value;
+
+  status = napi_create_int32(env, number, &value);
+  assert(status == napi_ok);
+
+  return napi_set_named_property(env, object, name, value);
+}
+
+napi_status objectSetUint32(napi_env env, napi_value object, const char *name,
+                            uint32_t number) {
+  napi_status status;
+  napi_value value;
+
+  status = napi_create_uint32(env, number, &value);
+  assert(status == napi_ok);
+
+  return napi_set_named_property(env, object, name, value);
+}
+
+napi_status objectSetInt64(napi_env env, napi_value object, const char *name,
+                           int64_t number) {
+  napi_status status;
+  napi_value value;
+
+  status = napi_create_int64(env, number, &value);
+  assert(status == napi_ok);
+
+  return napi_set_named_property(env, object, name, value);
+}
+
+napi_status objectSetDouble(napi_env env, napi_value object, const char *name,
+                            double number) {
+  napi_status status;
+  napi_value value;
+
+  status = napi_create_double(env, number, &value);
+  assert(status == napi_ok);
+
+  return napi_set_named_property(env, object, name, value);
 }
--- src/napi_ctp.h
+++ src/napi_ctp.h
@@ -37,6 +37,9 @@ typedef struct Message {
 
 #define arraysize(a) ((int)(sizeof(a) / sizeof(*a)))
 
+typedef napi_status (*MessageValueFunc)(napi_env env, const Message *message,
+                                        napi_value *result);
+
 #define DECLARE_NAPI_METHOD_(name, method)                                     \
   { name, 0, method, 0, 0, 0, napi_default, 0 }
 #define DECLARE_NAPI_METHOD(method) DECLARE_NAPI_METHOD_(#method, method)
@@ -53,6 +56,36 @@ napi_status defineClass(napi_env env, const char *name,
 napi_value createInstance(napi_env env, napi_callback_info info,
                           napi_ref constructor, size_t argc);
 
+napi_status objectSetString(napi_env env, napi_value object, const char *name,
+                            const char *string);
+
+napi_status objectSetInt32(napi_env env, napi_value object, const char *name,
+                           int32_t number);
+
+napi_status objectSetUint32(napi_env env, napi_value object, const char *name,
+                            uint32_t number);
+
+napi_status objectSetInt64(napi_env env, napi_value object, const char *name,
+                           int64_t number);
+
+napi_status objectSetDouble(napi_env env, napi_value object, const char *name,
+                            double number);
+
+#define ObjectString(env, object, record, name)                                \
+  objectSetString(env, object, #name, (const char *)record->##name)
+
+#define ObjectInt32(env, object, record, name)                                 \
+  objectSetInt32(env, object, #name, record->##name)
+
+#define ObjectUint32(env, object, record, name)                                \
+  objectSetUint32(env, object, #name, record->##name)
+
+#define ObjectInt64(env, object, record, name)                                 \
+  objectSetInt64(env, object, #name, record->##name)
+
+#define ObjectDouble(env, object, record, name)                                \
+  objectSetDouble(env, object, #name, record->##name)
+
 template <typename T> static inline uintptr_t copyData(T *data) {
   T *p = (T *)malloc(sizeof(T));
 
@@ -61,6 +94,10 @@ template <typename T> static inline uintptr_t copyData(T *data) {
 
   *p = *data;
   return (uintptr_t)p;
+}
+
+template <typename T> static inline T *MessageData(const Message *message) {
+  return (T *)message->data;
 }
 
 #endif /* __NAPI_CTP_H__ */
