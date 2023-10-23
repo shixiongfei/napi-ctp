279c4053508365082422e725be5af43706743678
shixiongfei <xiongfei.shi@icloud.com>
2023-10-23 21:56:05 +0800

    Guard类改名为DeferGuard

--- src/guard.h
+++ src/guard.h
@@ -18,16 +18,16 @@
 #define __QUOTE__(name, id) name##id
 #define _QUOTE(name, id) __QUOTE__(name, id)
 
-class Guard {
+class DeferGuard {
 public:
-  inline Guard(const std::function<void(void)> &func) : _func(func) {}
-  inline ~Guard() { _func(); }
+  inline DeferGuard(const std::function<void(void)> &func) : _func(func) {}
+  inline ~DeferGuard() { _func(); }
 
 private:
   std::function<void(void)> _func;
 };
 
-#define Defer Guard _QUOTE(__guard_, __LINE__)
+#define Defer DeferGuard _QUOTE(__guard_, __LINE__)
 
 class LockGuard {
 public:
