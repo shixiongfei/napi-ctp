2f0cddfbdc42eee90b76cdad415948b75035cee8
shixiongfei <xiongfei.shi@icloud.com>
2024-05-15 22:15:02 +0800

    修复symlink文件丢失问题

---
+++ prebuild.js
@@ -0,0 +1,39 @@
+/*
+ * prebuild.js
+ *
+ * Copyright (c) 2022-2024 Xiongfei Shi
+ *
+ * Author: Xiongfei Shi <xiongfei.shi(a)icloud.com>
+ * License: Apache-2.0
+ *
+ * https://github.com/shixiongfei/napi-ctp
+ */
+
+if (process.platform === "darwin") {
+  const fs = require("node:fs");
+
+  const symlinks = [
+    [
+      "./tradeapi/macos/thostmduserapi_se.framework/thostmduserapi_se",
+      "./Versions/Current",
+    ],
+    [
+      "./tradeapi/macos/thostmduserapi_se.framework/Versions/Current",
+      "./A/thostmduserapi_se",
+    ],
+    [
+      "./tradeapi/macos/thosttraderapi_se.framework/thosttraderapi_se",
+      "./Versions/Current",
+    ],
+    [
+      "./tradeapi/macos/thosttraderapi_se.framework/Versions/Current",
+      "./A/thosttraderapi_se",
+    ],
+  ];
+
+  symlinks.forEach(([symlink, target]) => {
+    if (!fs.existsSync(symlink)) {
+      fs.symlinkSync(target, symlink);
+    }
+  });
+}
