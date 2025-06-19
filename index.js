89c9d4e80a988c433113063ebf687214144d9901
shixiongfei <xiongfei.shi@icloud.com>
2025-06-19 12:11:40 +0800

    修复在node_modules中加载路径错误

--- index.js
+++ index.js
@@ -14,17 +14,28 @@ export * from "./types.js";
 
 import { dlopen } from "node:process";
 import { constants } from "node:os";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
 import * as types1 from "@napi-ctp/types";
 import * as types2 from "./types.js";
 
 const binding = () => {
+  const dir = dirname(fileURLToPath(import.meta.url));
   const module = { exports: {} };
 
   try {
-    dlopen(module, "./build/Release/napi_ctp.node", constants.dlopen.RTLD_LAZY);
+    dlopen(
+      module,
+      join(dir, "./build/Release/napi_ctp.node"),
+      constants.dlopen.RTLD_LAZY
+    );
   } catch {
     try {
-      dlopen(module, "./build/Debug/napi_ctp.node", constants.dlopen.RTLD_LAZY);
+      dlopen(
+        module,
+        join(dir, "./build/Debug/napi_ctp.node"),
+        constants.dlopen.RTLD_LAZY
+      );
     } catch {
       throw new Error("Cannot find module 'napi_ctp.node'");
     }
